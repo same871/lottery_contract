e14ed808eaa9996ffd615aefcc14d90e34429043
same871 <samueltakudzwamuto@gmail.com>
2023-05-23 15:09:45 +0200

    wrote deployscript for the contract and Mock contracts

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,25 @@
+const { ethers } = require("ethers")
+const { developmentChains } = require("../helper-hardhat-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is premium, it costs 0.25 LINK
+const GAS_PRICE_LINK = 1e9
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+        // deploy a mock vrf coodinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            args: args,
+            log: true,
+        })
+        log("Mocks deployed!")
+        log("___________________________________")
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
